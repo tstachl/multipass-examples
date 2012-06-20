7cf77d319828be7e875e53bc3a903f9aab8c642b
Joslyn Esser <joslyn.esser@gmail.com>
2012-06-20 14:01:04 -0700

    Updating python tender_multipass example for consistency with the base python example

--- python-tender_multipass.py
+++ python-tender_multipass.py
@@ -3,23 +3,24 @@
 # Install with pip: "pip install tender_multipass"
 
 import base64
-import datetime
 import hashlib
 import hmac
-import pytz
 import tender_multipass
 import urllib
+from datetime import datetime, timedelta
+from isodate import datetime_isoformat
 
 SITE_KEY = 'Your Site Key'
 API_KEY = 'Your API Key'
 
-expires = pytz.utc.localize(datetime.datetime.now() + 
-                            datetime.timedelta(minutes=2))
+expires = datetime_isoformat(datetime.utcnow() +
+                             timedelta(minutes=2))
+
 data = {
     'customer_name': 'John Doe',
     'customer_email': 'john.doe@yoursite.com',
     'uid': '123456',
-    'expires': expires.isoformat(),
+    'expires': expires,
 }
 multipass = tender_multipass.MultiPass(SITE_KEY, API_KEY)
 multipass_string = multipass.encode(data)
@@ -29,4 +30,6 @@ signature = hmac.new(API_KEY,
                      digestmod=hashlib.sha1)
 # urllib.quote() to mimic Ruby's CGI.escape
 # http://docs.python.org/library/urllib.html#urllib.quote
-signature_string = urllib.quote(base64.b64encode(signature.digest()))
+signature_string = urllib.quote(base64.b64encode(signature.digest()), '')
+
+print "Multipass: %s\nSignature: %s" % (multipass_string, signature_string)
