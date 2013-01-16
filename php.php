f133ea229bf56f8521fd332381c8f284d851e3f3
Joslyn Esser <joslyn@desk.com>
2013-01-16 14:38:07 -0800

    PHP example now supporting random IV generation

--- php.php
+++ php.php
@@ -5,7 +5,7 @@ $api_key     = 'YOUR MULTIPASS API KEY';
 $salted = $api_key . $account_key;
 $hash = hash('sha1',$salted,true);
 $saltedHash = substr($hash,0,16);
-$iv = "OpenSSL for Ruby";
+$iv = mcrypt_create_iv(16);
 
 // Build json data
 $user_data = array(
@@ -16,20 +16,19 @@ $user_data = array(
 );
 $data = json_encode($user_data);
 
-// XOR first block of data with IV
-for ($i = 0; $i < 16; $i++) {
-	$data[$i] = $data[$i] ^ $iv[$i];
-}
-
-// pad using standard PKCS#5 padding with block size of 16 bytes
+// Pad using standard PKCS#5 padding with block size of 16 bytes
 $pad = 16 - (strlen($data) % 16);
 $data = $data . str_repeat(chr($pad), $pad);
 
-// encrypt data using AES128-cbc
+// Encrypt data using AES128-cbc
 $cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128,'','cbc','');
 mcrypt_generic_init($cipher, $saltedHash, $iv);
 $multipass = mcrypt_generic($cipher,$data);
 mcrypt_generic_deinit($cipher);
+
+// Prepend the IV to the encrypted data
+// This will be extracted when decrypting
+$multipass = $iv . $multipass;
 
 // Base64 encode the encrypted data
 $multipass = base64_encode($multipass);
@@ -48,4 +47,7 @@ $signature = base64_encode($signature);
 // Finally, URL encode the multipass and signature
 $multipass = urlencode($multipass);
 $signature = urlencode($signature);
+
+print "multipass: " . $multipass . "\n";
+print "signature: " . $signature . "\n";
 ?>
