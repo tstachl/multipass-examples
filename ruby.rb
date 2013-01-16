5eee24584268b235baf29220b608bc2e304cb528
Joslyn Esser <joslyn@desk.com>
2013-01-16 15:17:04 -0800

    Ruby example now using random IV and standard core libraries

--- ruby.rb
+++ ruby.rb
@@ -1,23 +1,45 @@
-# Multipass example using entp's multipass gem (http://github.com/entp/multipass)
-#   Make sure to install the gem: "gem install multipass"
-
-require 'rubygems'
-require 'multipass'
 require 'openssl'
+require 'digest/sha1'
+require 'base64'
 require 'cgi'
 require 'time'
+require 'json'
 
-SITE_KEY = "Your Site Key"
-API_KEY = "Your API Key"
+SUBDOMAIN = "Your Desk.com Subdomain"
+API_KEY = "Your Multipass API Key"
 
-multipass_string = MultiPass.encode(
-  SITE_KEY,
-  API_KEY,
-  :customer_name => "John Doe",
-  :customer_email => "john.doe@yoursite.com",
+# Create the encryption key using a 16 byte SHA1 digest of your api key and subdomain
+key = Digest::SHA1.digest(API_KEY + SUBDOMAIN)[0...16]
+
+# Generate a random 16 byte IV
+iv = OpenSSL::Random.random_bytes(16)
+
+# Build the JSON string
+json = JSON.generate(
   :uid => "123456",
   :expires => (Time.now + 120).iso8601, # Expire two minutes from now
-  :url_safe => true # Convert unsafe characters
-)
+  :customer_name => "John Doe",
+  :customer_email => "john.doe@yoursite.com")
 
-signature = CGI.escape(Base64.encode64(OpenSSL::HMAC.digest('sha1', API_KEY, multipass_string)))
+# Encrypt JSON string using AES128-CBC
+cipher = OpenSSL::Cipher::Cipher.new("aes-128-cbc")
+cipher.encrypt # specifies the cipher's mode (encryption vs decryption)
+cipher.key = key
+cipher.iv = iv
+encrypted = cipher.update(json) + cipher.final
+
+# Prepend encrypted data with the IV
+prepended = iv + encrypted
+
+# Base64 encode the prepended encrypted data
+multipass = Base64.encode64(prepended)
+
+# Build an HMAC-SHA1 signature using the encoded multipass and your api key
+signature = Base64.encode64(OpenSSL::HMAC.digest('sha1', API_KEY, multipass))
+
+# URL escape the final multipass and signature parameters
+encoded_multipass = CGI.escape(multipass)
+encoded_signature = CGI.escape(signature)
+
+puts "multipass: #{encoded_multipass}"
+puts "signature: #{encoded_signature}"
