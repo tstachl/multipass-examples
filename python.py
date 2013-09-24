8c5f719cc12b681c3ebab6378ef59adc0fca462a
Thomas Stachl <thomas@stachl.me>
2013-09-23 17:24:38 -0700

    Update python.py

--- python.py
+++ python.py
@@ -2,67 +2,40 @@
 #   isodate
 #   pycrypto
 
-import json
-import hashlib
-import random
-from datetime import datetime, timedelta
-from base64 import b64encode
-
-from isodate import datetime_isoformat
+from Crypto import Random
 from Crypto.Cipher import AES
-import hmac
+from datetime import datetime, timedelta
+from isodate import datetime_isoformat
+import base64
 import hashlib
+import hmac
 import urllib
-
-ACCOUNT_KEY = 'YOUR SITE KEY'
-API_KEY = 'YOUR MULTIPASS API KEY'
-
-def multipass_string(user_id, user_name, user_email):
-	# Create the JSON for the multipass.
-	multipass_json = json.dumps({
-			'uid': user_id,
-			'expires': datetime_isoformat(
-							datetime.utcnow() + timedelta(minutes=2)),
-			'customer_email': user_email,
-			'customer_name': user_name
-		})
-
-	# Create the 16 byte salted hash.
-	salt = API_KEY + ACCOUNT_KEY
-	salted_hash = hashlib.sha1(salt).digest()[:16]
-
-	# Seed the random number generator with the current time, then create a 
-	# random Initialisation Vector (IV).
-	random.seed()
-	iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-	
-	# Pad the JSON to a multiple of 16 bytes using PKCS#5 padding.
-	padding_length = 16 - len(multipass_json) % 16
-	multipass_json += ''.ljust(padding_length, chr(padding_length))
-
-	# XOR the first 16-byte block with the IV.
-	first_block = ''
-	for i in range(16):
-		first_block += chr(ord(multipass_json[i]) ^ ord(iv[i]))
-	multipass_xored = first_block + multipass_json[16:]
-
-	# Encrypt the multipass using AES encryption in CBC mode.
-	encryptor = AES.new(salted_hash, AES.MODE_CBC, iv)
-	multipass_encrypted = encryptor.encrypt(multipass_xored)
-
-	# Encode the encrypted data using Base64 encoding
-	return b64encode(multipass_encrypted)
-
-def signature_string(multipass):
-	signature = hmac.new(API_KEY, multipass, hashlib.sha1).digest()
-	return b64encode(signature)
-
+import json
+ 
+SITE_NAME     = 'your_desk_site_name'
+MULTIPASS_KEY = 'your_multipass_key'
+ 
+def pad(string, length):
+  return string + (length - len(string) % length) * chr(length - len(string) % length) 
+ 
+def multipass(json_data):
+  salt  = hashlib.sha1(MULTIPASS_KEY + SITE_NAME).digest()[:16]
+  iv    = Random.new().read(AES.block_size)
+  aes   = AES.new(salt, AES.MODE_CBC, iv)
+  multi = aes.encrypt(pad(json_data, AES.block_size))
+  return base64.b64encode(iv + multi)
+ 
+def signature(multipass):
+  signature = hmac.new(MULTIPASS_KEY, multipass, hashlib.sha1).digest()
+  return base64.b64encode(signature)
+ 
 if __name__ == '__main__':
-	multipass = multipass_string('0123457', 'Jan Anonymous', 'jan@anon.anon')
-	signature = signature_string(multipass)
-
-	# URL encode the multipass and signature parameters
-	multipass = urllib.quote(multipass, '')
-	signature = urllib.quote(signature, '')
-
-	print "Multipass: %s\nSignature: %s" % (multipass, signature)
+  mp  = multipass(json.dumps({
+    'uid': '123',
+    'customer_email': 'testuser@yoursite.com',
+    'customer_name': 'Test User',
+    'expires': datetime_isoformat(datetime.utcnow() + timedelta(minutes = 2))
+    }))
+  sg  = signature(mp)
+  url = "https://devel.desk.com/customer/authentication/multipass/callback?multipass=%s&signature=%s"
+  print url % (urllib.quote(mp), urllib.quote(sg))
