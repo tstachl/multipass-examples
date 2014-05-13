55ab96f3c20f242c06608e12e67ae4c98b39e69c
Thomas Stachl <thomas@stachl.me>
2014-05-13 11:49:12 -0700

    Update c-sharp.cs
    This example actually works.

--- c-sharp.cs
+++ c-sharp.cs
@@ -1,102 +1,122 @@
-// Assistly C# Multipass Example
-// Contributed by Bryan Siders: http://sypher-news.blogspot.com/2011/03/assistly-multipass-sso.html
-// Note: This example was contributed from a third party and has not been tested at Assistly
-
 using System;
-using System.Configuration;
-using System.IO;
-using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.Web.Script.Serialization;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Web;
+using System.Diagnostics;
+using Newtonsoft.Json;
 
-namespace Sypher.Multipass
+namespace ConsoleApplication1
 {
-    public partial class _Login : System.Web.UI.Page
+    class Program
     {
-        protected class UserData
+        public const string site_key = "your_site_name";
+        private const string api_key = "your_api_key";
+
+        static byte[] Encrypt(string json, byte[] Key, byte[] IV)
         {
-            public string uid;
-            public string expires;
-            public string customer_email;
-            public string customer_name;
-            public string customer_custom_siteid;    // custom fields
-            public string customer_custom_sitename;    // add your own as needed
-        }
+            byte[] encrypted;
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            UserData user_data = new UserData();
-
-            // Set these values however you need
-            user_data.uid = "12345678-0000-0000-0000-123456789012";
-            user_data.expires = DateTime.UtcNow.AddMinutes(2).ToString("yyyy-MM-ddTHH:mm:sszzz"); // ISO 8601 like 2011-12-29T10:25:28-08:00
-            user_data.customer_email = "testing@testing.com";
-            user_data.customer_name = "Test User";
-            user_data.customer_custom_siteid = "01234567-0000-0000-0000-890123456789";
-            user_data.customer_custom_sitename = "Test Organization";
-
-            string assistly_url = string.Format("http://{0}.assistly.com/customer/authentication/multipass/callback?multipass={1}",
-                ConfigurationManager.AppSettings["assistly-site-key"], // See encryptUserData for appSettings example
-                encryptUserData(user_data));
-
-            Response.Redirect(assistly_url);
-        }
-
-        protected static string encryptUserData(UserData user_data)
-        {
-            // Encode the data into a JSON object
-            JavaScriptSerializer s = new JavaScriptSerializer();
-            string json_data = s.Serialize(user_data);
-
-            // Example of web.config configuration/appSettings section:
-            // <add key="assistly-site-key" value="sitename" /> <!-- from sitename.assistly.com -->
-            // <add key="assistly-api-key" value="0123456789abcdef0123456789abcdef" />
-            // <add key="assistly-iv" value="OpenSSL for Ruby" /> <!-- Static value from Assistly -->
-            string site_key = ConfigurationManager.AppSettings["assistly-site-key"];
-            string api_key = ConfigurationManager.AppSettings["assistly-api-key"];
-            string iv = ConfigurationManager.AppSettings["assistly-iv"];
-
-            // Using byte arrays now instead of strings
-            byte[] encrypted = null;
-            byte[] bIV = Encoding.ASCII.GetBytes(iv);
-            byte[] data = Encoding.ASCII.GetBytes(json_data);
-
-            // XOR the first block (16 bytes) 
-            // once before the full XOR
-            // so it gets double XORed
-            for (var i = 0; i < 16; i++)
-                data[i] = (byte)(data[i] ^ bIV[i]);
-
-            // Use the AesManaged object to do the encryption
             using (AesManaged aesAlg = new AesManaged())
             {
-                aesAlg.IV = bIV;
-                aesAlg.KeySize = 16 * 8; // = 128-bit, originally defaulted to 256
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
 
-                // Create the 16-byte salted hash
-                SHA1 sha1 = SHA1.Create();
-                byte[] saltedHash = sha1.ComputeHash(Encoding.ASCII.GetBytes(api_key + site_key), 0, (api_key + site_key).Length);
-                Array.Resize(ref saltedHash, 16);
-                aesAlg.Key = saltedHash;
-
-                // Encrypt using the AES Managed object
+                // Create a decryptor to perform the stream transform
                 ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
                 using (MemoryStream msEncrypt = new MemoryStream())
                 {
                     using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
-                        csEncrypt.Write(data, 0, data.Length);
-                        csEncrypt.FlushFinalBlock();
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(json);
+                        }
+
+                        encrypted = msEncrypt.ToArray();
                     }
-                    encrypted = msEncrypt.ToArray();
                 }
             }
 
-            // Return the Base64-encoded encrypted data
-            return Convert.ToBase64String(encrypted, Base64FormattingOptions.None)
-                .TrimEnd("=".ToCharArray())    // Remove trailing equal (=) characters
-                .Replace("+", "-")            // Change any plus (+) characters to dashes (-)
-                .Replace("/", "_");            // Change any slashes (/) characters to underscores (_)
+            return encrypted;
+        }
+
+        static byte[] EncryptionKey()
+        {
+            byte[] key;
+            byte[] salt = Encoding.UTF8.GetBytes(api_key + site_key);
+
+            using (SHA1 sha1 = new SHA1CryptoServiceProvider())
+            {
+                key = sha1.ComputeHash(salt);
+                Array.Resize(ref key, 16);
+            }
+
+            return key;
+        }
+
+        static byte[] Signature(string multipass)
+        {
+            byte[] signature;
+
+            using (HMACSHA1 hmac = new HMACSHA1(Encoding.UTF8.GetBytes(api_key)))
+            {
+                using (MemoryStream msHmac = new MemoryStream(Encoding.UTF8.GetBytes(multipass)))
+                {
+                    signature = hmac.ComputeHash(msHmac);
+                }
+            }
+
+            return signature;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Debug.WriteLine("== Generating ==");
+
+                Debug.WriteLine("    Build json data");
+                var json = JsonConvert.SerializeObject(new Dictionary<string, string>(){
+                    {"uid", "19238333"},
+                    {"expires", DateTime.UtcNow.AddMinutes(10).ToString("o")},
+                    {"customer_email", "john@example.com"},
+                    {"customer_name", "John"}
+                });
+                Debug.WriteLine("    Data: {0}", json);
+
+                using (AesManaged myAes = new AesManaged())
+                {
+                    byte[] encrypted = Encrypt(json, EncryptionKey(), myAes.IV);
+
+                    Debug.WriteLine("    Prepend the IV to the encrypted data");
+                    byte[] combined = new byte[myAes.IV.Length + encrypted.Length];
+                    Array.Copy(myAes.IV, 0, combined, 0, myAes.IV.Length);
+                    Array.Copy(encrypted, 0, combined, myAes.IV.Length, encrypted.Length);
+
+                    Debug.WriteLine("    Base64 encode the encrypted data");
+                    var multipass = Convert.ToBase64String(combined);
+
+                    Debug.WriteLine("    Build an HMAC-SHA1 signature using the encoded string and your api key");
+                    byte[] encrypted_signature = Signature(multipass);
+                    var signature = Convert.ToBase64String(encrypted_signature);
+
+                    Debug.WriteLine("    Finally, URL encode the multipass and signature");
+                    multipass = Uri.EscapeDataString(multipass);
+                    signature = Uri.EscapeDataString(signature);
+
+                    Debug.WriteLine("== Finished ==");
+                    Debug.WriteLine("https://{0}.desk.com/customer/authentication/multipass/callback?multipass={1}&signature={2}", site_key, multipass, signature);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception {0} \n{1}", e.Message, e.StackTrace);
+            }
         }
     }
 }
